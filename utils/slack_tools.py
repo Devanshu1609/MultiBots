e97d50c649a054fb55277952cfcc556f542143e5
Devanshu1609 <devanshukumarsingh03@gmail.com>
2026-02-03 02:47:24 +0530

    Tools defined to be used in slack_bot

---
+++ utils/slack_tools.py
@@ -0,0 +1,19 @@
+from langchain_core.tools import Tool
+from slack_sdk import WebClient
+import os
+
+slack_token = os.getenv("SLACK_BOT_TOKEN")
+slack_client = WebClient(token=slack_token)
+
+def get_channel_info(channel_name: str) -> str:
+    """Get info about a Slack channel by name. Input should be a channel name like #general"""
+    try:
+        response = slack_client.conversations_list()
+        channels = response["channels"]
+        for channel in channels:
+            if channel["name"] == channel_name.lstrip("#"):
+                return str(channel)
+        return f"Channel '{channel_name}' not found."
+    except Exception as e:
+        return f"Error: {e}"
+
